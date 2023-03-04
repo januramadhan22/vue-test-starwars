43f96df54d80e2110fea29943980f15520d50f85
Janu <abbyjunior600@gmail.com>
2023-03-04 09:29:21 +0700

    install tools

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is home <span>askdjakd</span></h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+h1 {
+  color: red;
+
+  span {
+    color: blue;
+  }
+}
+</style>
